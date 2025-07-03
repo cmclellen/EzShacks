58f3d077ff3fa9960ad6b6927bb9d6d43a53fbd1
Craig McLellen <cmclellen@gmail.com>
2025-07-04 07:21:02 +0800

    Fix

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
 import clsx from "clsx";
 import dynamic from "next/dynamic";
+import { useState } from "react";
+import { HiBars3, HiBell, HiXMark } from "react-icons/hi2";
 
 const DarkMode = dynamic(() => import("./DarkMode"), {
   ssr: false,
@@ -12,28 +23,58 @@ export const revalidate = 0;
 type NavItemProps = {
   readonly children: React.ReactNode;
   readonly isPadded?: boolean;
+  readonly className?: string;
 };
 
-function NavItem({ children, isPadded = true }: NavItemProps) {
+function NavItem({ children, className, isPadded = true }: NavItemProps) {
   return (
     <li
-      className={clsx("rounded-md cursor-pointer z-10", {
-        "hover:bg-primary hover:text-on-primary px-3 py-1": isPadded,
-      })}
+      className={clsx(
+        className,
+        "rounded-md cursor-pointer z-10 w-full md:w-auto text-center",
+        {
+          "hover:bg-primary hover:text-on-primary py-3 md:px-3 md:py-1":
+            isPadded,
+        }
+      )}
     >
       {children}
     </li>
   );
 }
 
-function Navigation() {
+type NavigationProps = {
+  readonly children?: React.ReactNode;
+};
+
+function Navigation({ children }: NavigationProps) {
+  const [open, setOpen] = useState(false);
   return (
-    <nav>
-      <ul className="flex items-center gap-4 text-sm font-semibold">
+    <nav className="relative z-20 w-screen flex items-center justify-between">
+      <div>{children}</div>
+      <button
+        className="md:hidden flex flex-col gap-1 items-center px-2 py-1 rounded focus:outline-none"
+        aria-label="Toggle navigation"
+        onClick={() => setOpen((v) => !v)}
+      >
+        <span className="block w-6 h-0.5 bg-current"></span>
+        <span className="block w-6 h-0.5 bg-current"></span>
+        <span className="block w-6 h-0.5 bg-current"></span>
+      </button>
+
+      <ul
+        className={clsx(
+          "flex p-0 rounded-md md:flex-row items-center text-sm font-semibold bg-surface absolute left-0 right-0 top-15 shadow transition-all duration-200 md:static md:top-0 md:shadow-none md:flex md:gap-4 md:bg-transparent",
+          {
+            "flex-col": open,
+            hidden: !open,
+          }
+        )}
+      >
         <NavItem>Shacks</NavItem>
         <NavItem>About</NavItem>
         <NavItem>Sign in</NavItem>
-        <NavItem isPadded={false}>
+        <NavItem isPadded={false} className="hidden md:flex">
           <DarkMode />
         </NavItem>
       </ul>
