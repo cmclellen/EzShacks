3f252d23635068635f36e670ecec3b0b2f0b9aa9
Craig McLellen <cmclellen@gmail.com>
2025-07-12 06:43:41 +0800

    Fix

--- app/shacks/[shackId]/page.tsx
+++ app/shacks/[shackId]/page.tsx
@@ -1,7 +1,9 @@
 import DateSelector from "@/app/_components/DateSelector";
+import LoginMessage from "@/app/_components/LoginMessage";
 import ReservationForm from "@/app/_components/ReservationForm";
 import ShackDefault from "@/app/_components/ShackDefault";
 import { getBookedDatesByShackId, getShack } from "@/app/_lib/apiShacks";
+import { auth } from "@/app/_lib/auth";
 import { Params } from "next/dist/server/request/params";
 
 type PageProps = {
@@ -13,6 +15,7 @@ async function Page({ params }: PageProps) {
   const [bookedDates] = await Promise.all([
     getBookedDatesByShackId(Number(shackId)),
   ]);
+  const session: any = await auth();
 
   const shack = await getShack(Number(shackId));
 
@@ -27,7 +30,11 @@ async function Page({ params }: PageProps) {
 
         <div className="grid grid-cols-2 border min-h-[400px]">
           <DateSelector bookedDates={bookedDates} shack={shack} />
-          <ReservationForm shack={shack} />
+          {session?.user ? (
+            <ReservationForm shack={shack} user={session!.user} />
+          ) : (
+            <LoginMessage />
+          )}
         </div>
       </div>
     </div>
